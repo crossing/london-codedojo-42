0b5ba4fe7f83f14339edb3911bf5e9c53c828212
Xing Yang <xor@jecity.net>
2017-02-21 09:46:32 +0000

    Implemented new requirements

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,6 +1,30 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer [update-quality item]]))
 
-(describe "gilded rose"
-          (it "TODO - write tests later"))
+(describe "Backstage passes"
+          (it "quality drops to 0 after concert is over"
+              (let [backstage-pass (item "Backstage passes to a TAFKAL80ETC concert" -1 50)
+                    [updated] (update-quality [backstage-pass])]
+                (should (= 0 (:quality updated))))))
+
+(describe "Conjure items"
+          (it "degrades twice as fast as normal items"
+              (let [conjured-item (item "Conjured item" 10 10)
+                    [updated] (update-quality [conjured-item])]
+                (should= 8 (:quality updated))))
+          (it "quality never drops below 0"
+              (let [conjured-item (item "Conjured item" 10 0)
+                    [updated] (update-quality [conjured-item])]
+                (should= 0 (:quality updated))))
+          (it "degrades twice as fast as before after sell in date"
+              (let [conjured-item (item "Conjured item" -1 10)
+                    [updated] (update-quality [conjured-item])]
+                (should= 6 (:quality updated))))
+          (it "sell-in value decreases"
+              (let [conjured-item (item "Conjured item" 2 10)
+                    [updated] (update-quality [conjured-item])]
+                (should= 1 (:sell-in updated)))))
+
+
+
--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -1,44 +1,58 @@
 (ns gilded-rose.core)
 
+(def ^:const backstage-pass-name "Backstage passes to a TAFKAL80ETC concert")
+(def ^:const elixir-of-mongoose "Elixir of the Mongoose")
+(def ^:const dexterity-vest "+5 Dexterity Vest")
+(def ^:const sulfuras "Sulfuras, Hand of Ragnaros")
+(def ^:const aged-brie "Aged Brie")
+
+(defn- update-quality-for-conjured-item
+  [item]
+  (let [decrease-in-quality (if (< (:sell-in item) 0) 4 2)]
+    (update-in item [:quality] #(max 0 (- % decrease-in-quality)))))
+
+(defn- conjured?
+  [item]
+  (= (:name item) "Conjured item"))
+
 (defn update-quality [items]
   (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
-            (if (< (:quality item) 50)
-              (merge item {:quality (inc (:quality item))})
-              item)))
-      (< (:sell-in item) 0)
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-          (merge item {:quality 0})
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
+   (fn [item] (cond
+                (conjured? item)
+                (update-quality-for-conjured-item item)
+                (and (< (:sell-in item) 0) (= backstage-pass-name (:name item)))
+                (merge item {:quality 0})
+                (or (= (:name item) aged-brie) (= (:name item) backstage-pass-name))
+                (if (and (= (:name item) backstage-pass-name) (>= (:sell-in item) 5) (< (:sell-in item) 10))
+                  (merge item {:quality (inc (inc (:quality item)))})
+                  (if (and (= (:name item) backstage-pass-name) (>= (:sell-in item) 0) (< (:sell-in item) 5))
+                    (merge item {:quality (inc (inc (inc (:quality item))))})
+                    (if (< (:quality item) 50)
+                      (merge item {:quality (inc (:quality item))})
+                      item)))
+                (< (:sell-in item) 0)
+                (if (= backstage-pass-name (:name item))
+                  (merge item {:quality 0})
+                  (if (or (= dexterity-vest (:name item)) (= elixir-of-mongoose (:name item)))
+                    (merge item {:quality (- (:quality item) 2)})
+                    item))
+                (or (= dexterity-vest (:name item)) (= elixir-of-mongoose (:name item)))
+                (merge item {:quality (dec (:quality item))})
+                :else item))
+   (map (fn [item]
+          (if (not= sulfuras (:name item))
+            (merge item {:sell-in (dec (:sell-in item))})
             item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+        items)))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
-(defn update-current-inventory[]
-  (let [inventory 
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+(defn update-current-inventory []
+  (let [inventory
+        [(item dexterity-vest 10 20)
+         (item aged-brie 2 0)
+         (item elixir-of-mongoose 5 7)
+         (item sulfuras 0 80)
+         (item backstage-pass-name 15 20)]]
+    (update-quality inventory)))
